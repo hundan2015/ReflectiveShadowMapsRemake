e2c755116dda9e88b5ca1427ec9978c89559e288
hundan2015 <sym12138@hotmail.com>
2023-06-02 00:25:05 +0800

    make a temp version2. It now can get some ShaderProgram, but not tested.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 #include "GLFW/glfw3.h"
+#include "controller/Engine.h"
 #include "controller/ResourceManager.h"
 #include "model/GameObject.h"
 #include "model/Mesh.h"
@@ -14,27 +15,8 @@ const unsigned int kScrWidth = 512;
 const unsigned int kScrHeight = 512;
 using namespace renderer;
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window =
-        glfwCreateWindow(kScrWidth, kScrHeight, "LearnOpenGL", NULL, NULL);
-    if (window == nullptr) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
     GameObject camera;
+    Engine engine;
 
     Mesh mesh(ResourceManager::GetMeshVao("dir"));
     Material material = {ResourceManager::GetTexture("Albedo"),
@@ -43,66 +25,6 @@ int main() {
                          ResourceManager::GetShaderProgram("fs", "vs")};
     mesh.SetMaterial(material);
 
-
-    while (!glfwWindowShouldClose(window)) {
-        // 1. 几何处理阶段
-        GLuint g_buffer;
-        glGenFramebuffers(1, &g_buffer);
-        glBindFramebuffer(GL_FRAMEBUFFER, g_buffer);
-        GLuint g_position;
-        GLuint g_normal;
-        GLuint g_albedo_spec;
-        // 位置颜色缓冲
-        glGenTextures(1, &g_position);
-        glBindTexture(GL_TEXTURE_2D, g_position);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, kScrWidth, kScrHeight, 0,
-                     GL_RGB, GL_FLOAT, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                               GL_TEXTURE_2D, g_position, 0);
-        // 法线颜色缓冲
-        glGenTextures(1, &g_normal);
-        glBindTexture(GL_TEXTURE_2D, g_normal);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, kScrWidth, kScrHeight, 0,
-                     GL_RGB, GL_FLOAT, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1,
-                               GL_TEXTURE_2D, g_normal, 0);
-        // 颜色+镜面颜色缓冲
-        glGenTextures(1, &g_albedo_spec);
-        glBindTexture(GL_TEXTURE_2D, g_normal);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, kScrWidth, kScrHeight, 0,
-                     GL_RGBA, GL_FLOAT, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2,
-                               GL_TEXTURE_2D, g_albedo_spec, 0);
-
-        GLuint attachments[] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1,
-                                GL_COLOR_ATTACHMENT2};
-        glDrawBuffers(3, attachments);
-
-        glClearColor(0.2F, 0.3F, 0.3F, 1.0F);
-        glClear(GL_COLOR_BUFFER_BIT);
-        // TODO: Use the first pass shader;
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, g_position);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, g_normal);
-        glActiveTexture(GL_TEXTURE2);
-        glBindTexture(GL_TEXTURE_2D, g_albedo_spec);
-        // TODO: Send light data to shader;
-        auto camera_position = camera.GetPosition();
-        const unsigned int shader_lighting_pass_program = 0;
-        glUniform3fv(
-            glGetUniformLocation(shader_lighting_pass_program, "view_pos"), 1,
-            &camera_position[0]);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    engine.Start();
     return 0;
 }
--- src/src/controller/ResoureManager.cpp
+++ src/src/controller/ResoureManager.cpp
@@ -1,5 +1,74 @@
 //
 // Created by Symbolic on 2023/6/1.
 //
-
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#endif
 #include "controller/ResourceManager.h"
+#include "stb_image.h"
+namespace renderer {
+unsigned int ResourceManager::GetShaderProgram(
+    const std::string& vertex_shader_dir,
+    const std::string& fragment_shader_dir) {
+    // 创建Fragment shader
+    std::string fragment_shader_source = GetStringFromFile(fragment_shader_dir);
+    unsigned int fragment_shader;
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* data = fragment_shader_source.c_str();
+    glShaderSource(fragment_shader, 1, &data, nullptr);
+    // 创建Vertex shader
+    std::string vertex_shader_source = GetStringFromFile(vertex_shader_dir);
+    unsigned int vertex_shader;
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertex_data = vertex_shader_source.c_str();
+    glShaderSource(vertex_shader, 1, &vertex_data, nullptr);
+    // 创建Shader program
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    return shader_program;
+}
+std::string ResourceManager::GetStringFromFile(const std::string& dir) {
+    std::ifstream file_stream;
+    file_stream.open(dir);
+    std::stringstream string_stream;
+    string_stream << file_stream.rdbuf();
+    std::string source = string_stream.str();
+    return source;
+}
+unsigned int ResourceManager::GetTexture(const std::string& texture_dir) {
+    unsigned int texture;
+    int width;
+    int height;
+    int nr_channel;
+    unsigned char* data = nullptr;
+    const char* directory_str = texture_dir.c_str();
+    data = stbi_load(directory_str, &width, &height, &nr_channel, 0);
+    assert(data != nullptr);
+    if (data == nullptr) {
+        std::cout << "The texture failed ot load!\n";
+    } else {
+        std::cout << "Loaded texture." << std::endl;
+    }
+    glGenTextures(1, &texture);
+    std::cout << "Texture id" << texture << std::endl;
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+
+    // glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+    return texture;
+}
+}  // namespace renderer
